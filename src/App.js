cbc8a75245adac9501e9dbebfa9ab1ccd4e4e80d
Matthew Nascimento <matthewromie@hotmail.com>
2021-06-09 14:43:42 -0300

    Dados da API

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React, { useState } from 'react';
 import Container from './components/Container/Container.jsx';
 import PokemonCard from './components/PokemonCard/PokemonCard.jsx';
 import Titulo from './components/Titulo/Titulo.jsx';
-
+import axios from 'axios';
 
 function App() {
 
   const [pokemon, setPokemon] = useState([]);
 
+  axios.get("https://pokeapi.co/api/v2/pokemon/").then((res) => {
+    setPokemon(res.data.results.map(p => p.name))
+  })
+  console.log(pokemon)
 
   return (
     <div className="App">
