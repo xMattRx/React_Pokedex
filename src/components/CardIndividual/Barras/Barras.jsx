ad58b90e33228de9fbe6ca67ff6a71c0553533f8
Matthew Nascimento <matthewromie@hotmail.com>
2021-06-12 16:28:03 -0300

    Finalizado

--- src/components/CardIndividual/Barras/Barras.jsx
+++ src/components/CardIndividual/Barras/Barras.jsx
@@ -12,56 +12,38 @@ export default function Barras({
   return (
     <div className="Barras">
       <div className="Barra">
-        <div
-          style={{ width: `${hp}%`, backgroundColor: "#53ff1a" }}
-          className="Preenchendo"
-        >
-          HP {hp}%
+        <div style={{ backgroundColor: "#53ff1a" }} className="Preenchendo">
+          HP {hp}
         </div>
       </div>
 
       <div className="Barra">
-        <div
-          style={{ width: `${attack}%`, backgroundColor: "#ff0000" }}
-          className="Preenchendo"
-        >
-          ATTACK {attack}%
+        <div style={{ backgroundColor: "#ff0000" }} className="Preenchendo">
+          ATTACK {attack}
         </div>
       </div>
 
       <div className="Barra">
-        <div
-          style={{ width: `${defense}%`, backgroundColor: "#3366ff" }}
-          className="Preenchendo"
-        >
-          DEFENSE {defense}%
+        <div style={{ backgroundColor: "#3366ff" }} className="Preenchendo">
+          DEFENSE {defense}
         </div>
       </div>
 
       <div className="Barra">
-        <div
-          style={{ width: `${speed}%`, backgroundColor: "#ffff00" }}
-          className="Preenchendo"
-        >
-          SPEED {speed}%
+        <div style={{ backgroundColor: "#ffff00" }} className="Preenchendo">
+          SPEED {speed}
         </div>
       </div>
 
       <div className="Barra">
-        <div
-          style={{ width: `${special_attack}%`, backgroundColor: "#ff9900" }}
-          className="Preenchendo"
-        >
-          SPECIAL ATTACK {special_attack}%
+        <div style={{ backgroundColor: "#ff9900" }} className="Preenchendo">
+          SPECIAL ATTACK {special_attack}
         </div>
       </div>
 
       <div className="Barra">
-        <div
-          style={{ width: `${special_defense}%`, backgroundColor: "#e600e6" }}
-          className="Preenchendo"
-        >
-          SPECIAL DEFENSE {special_defense}%
+        <div style={{ backgroundColor: "#e600e6" }} className="Preenchendo">
+          SPECIAL DEFENSE {special_defense}
         </div>
       </div>
     </div>
